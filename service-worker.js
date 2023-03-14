6510422b89af6b55acb0ad80dbf940dbef460f44
Gabriela Simeonova <g.simeonova@student.fontys.nl>
2023-03-14 20:01:32 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.c149a74f41db4467c0a3008038135a68.js"
+  "/GabriellaSimeonova/Happyabra-Mood-tracking-app/blob/gh-pages/index.html/precache-manifest.4751bca0638d3c2edb42298efc6cc9d6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-app/index.html", {
+workbox.routing.registerNavigationRoute("/GabriellaSimeonova/Happyabra-Mood-tracking-app/blob/gh-pages/index.html/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
