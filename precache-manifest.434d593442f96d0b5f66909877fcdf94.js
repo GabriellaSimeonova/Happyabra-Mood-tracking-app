210c5415bd823bc5729ab8c6299482853973bb0b
Gabriela Simeonova <g.simeonova@student.fontys.nl>
2023-03-14 20:11:30 +0100

    Updates

---
+++ precache-manifest.434d593442f96d0b5f66909877fcdf94.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f798cc4323bd0347f97",
+    "url": "/static/css/main.697bb570.chunk.css"
+  },
+  {
+    "revision": "0f798cc4323bd0347f97",
+    "url": "/static/js/main.6d930038.chunk.js"
+  },
+  {
+    "revision": "5d6637068433c7250bd2",
+    "url": "/static/js/runtime~main.02a69d94.js"
+  },
+  {
+    "revision": "722125f1ca3b73a1db28",
+    "url": "/static/js/2.02fa49f9.chunk.js"
+  },
+  {
+    "revision": "df6ea542164f30206c4d",
+    "url": "/static/js/3.57b001d4.chunk.js"
+  },
+  {
+    "revision": "1989bf298239e85231ebbdf2e9f5581f",
+    "url": "/static/media/camera.1989bf29.png"
+  },
+  {
+    "revision": "f9df6d623652c7495b996b574762b561",
+    "url": "/static/media/Jabi_San.f9df6d62.jpg"
+  },
+  {
+    "revision": "036c088ed5bd9ddbca98257d6226d28d",
+    "url": "/static/media/Fashionista.036c088e.jpeg"
+  },
+  {
+    "revision": "d56fd5ccf48faa4170f649e5864fe383",
+    "url": "/static/media/Jabimodo.d56fd5cc.jpeg"
+  },
+  {
+    "revision": "92c602893c40eecf144a79ddbf211128",
+    "url": "/static/media/arrow.92c60289.png"
+  },
+  {
+    "revision": "74920347420319fd12fcab4d883a0f88",
+    "url": "/static/media/offline.74920347.png"
+  },
+  {
+    "revision": "440dccbd34ac5bc2899bc4162e532002",
+    "url": "/index.html"
+  }
+];
